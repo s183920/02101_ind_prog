d4a8c992e253262ef7c726c85abe2e5ce9d98ae2
Lukas Leindals <s183920@student.dtu.dk>
2020-09-16 21:09:51 +0200

    add assigments

---
+++ Project1/src/Remember.java
@@ -0,0 +1,11 @@
+public class Remember {
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Remember:");
+        printLine();
+    }
+
+    public static void printLine(){
+        System.out.println("Use \"\\\\\" to obtain a \'backslash\' character.");
+    }
+}
---
+++ Project1/src/UltimateQuestion.java
@@ -0,0 +1,6 @@
+public class UltimateQuestion {
+    public static void main(String[] args) {
+        String x = "";
+        System.out.println(1 + 3 + x + x + x + x + x + 5 / 4 * 2);
+    }
+}
