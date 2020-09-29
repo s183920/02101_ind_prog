337d073a16842cfbe45a6a05be38b35a5fcd0956
Lukas Leindals <s183920@student.dtu.dk>
2020-09-29 11:47:02 +0200

    BuffonsNeedle

---
+++ Project2/src/BuffonsNeedle.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class BuffonsNeedle {
+    public static void main(String[] args) {
+        // define variables
+        int iter, succes = 0;
+        Random distance = new Random(), angle = new Random();
+        float d, a;
+
+        // get number of iterations and make sure it is a positive integer
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter number of iterations: ");
+        if (console.hasNextInt()) {
+            iter = console.nextInt();
+            if (iter <= 0){
+                throw new IllegalArgumentException("Number of iterations must be a positive integer");
+            }
+        } else {
+            throw new IllegalArgumentException("Only integers can be used for number of iterations.");
+        }
+
+        // calculate pi
+        for (int i = 0; i < iter; i++){
+            d = distance.nextFloat()*2;
+            a = angle.nextInt(181);
+            if (d+Math.sin(Math.toRadians(a)) >= 2){
+                succes += 1;
+            }
+        }
+        System.out.printf("%d / %d = %f", iter, succes, (float)iter/succes);
+    }
+}
+
